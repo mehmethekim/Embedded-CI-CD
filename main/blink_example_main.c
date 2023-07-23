cbca582ddab91f9cbb3cf461cbc95d62736c3d46
Mehmet Hekimoglu <mehmet.hekimoglu@kuartismed.com>
2023-07-23 12:01:39 +0300

    Added new files for blink in esp 5.1

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -23,20 +23,21 @@ static const char *TAG = "example";
 
 static uint8_t s_led_state = 0;
 
-#ifdef CONFIG_BLINK_LED_RMT
-static led_strip_t *pStrip_a;
+#ifdef CONFIG_BLINK_LED_STRIP
+
+static led_strip_handle_t led_strip;
 
 static void blink_led(void)
 {
     /* If the addressable LED is enabled */
     if (s_led_state) {
         /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        pStrip_a->set_pixel(pStrip_a, 0, 16, 16, 16);
+        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
         /* Refresh the strip to send data */
-        pStrip_a->refresh(pStrip_a, 100);
+        led_strip_refresh(led_strip);
     } else {
         /* Set all LED off to clear all pixels */
-        pStrip_a->clear(pStrip_a, 50);
+        led_strip_clear(led_strip);
     }
 }
 
@@ -44,9 +45,27 @@ static void configure_led(void)
 {
     ESP_LOGI(TAG, "Example configured to blink addressable LED!");
     /* LED strip initialization with the GPIO and pixels number*/
-    pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 1);
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,
+        .max_leds = 1, // at least one LED on board
+    };
+#if CONFIG_BLINK_LED_STRIP_BACKEND_RMT
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+        .flags.with_dma = false,
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+#elif CONFIG_BLINK_LED_STRIP_BACKEND_SPI
+    led_strip_spi_config_t spi_config = {
+        .spi_bus = SPI2_HOST,
+        .flags.with_dma = true,
+    };
+    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
+#else
+#error "unsupported LED strip backend"
+#endif
     /* Set all LED off to clear all pixels */
-    pStrip_a->clear(pStrip_a, 50);
+    led_strip_clear(led_strip);
 }
 
 #elif CONFIG_BLINK_LED_GPIO
@@ -65,6 +84,8 @@ static void configure_led(void)
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
 
+#else
+#error "unsupported LED type"
 #endif
 
 void app_main(void)
